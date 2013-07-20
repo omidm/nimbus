3caab1433dc94ceca6de8f81e4fc975252768c60
Philip Levis <pal@phoenix-2.local>
2013-07-20 15:35:45 -0700

    Clean up cluster.cc for cclint.

--- lib/cluster.cc
+++ lib/cluster.cc
@@ -1,44 +1,69 @@
+/*
+ * Copyright 2013 Stanford University.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * - Neither the name of the copyright holders nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+ /*
+  * Nimbus cheduler. 
+  *
+  * Author: Omid Mashayekhi <omidm@stanford.edu>
+  */
+
 #include "cluster.h"
 
-Node::Node()
-{
-};
+Node::Node() {}
 
-Node::~Node()
-{
-};
+Node::~Node() {}
 
-NodeType Node::getType()
-{
-  return COMPUTER; 
-};
-
-
-Computer::Computer()
-{
-};
-
-Computer::~Computer()
-{
-};
-
-NodeType Computer::getType()
-{
+NodeType Node::getType() {
   return COMPUTER;
-};
+}
 
-Switch::Switch()
-{
-};
 
-Switch::~Switch()
-{
-};
+Computer::Computer() {}
 
-NodeType Switch::getType()
-{
+Computer::~Computer() {}
+
+NodeType Computer::getType() {
   return COMPUTER;
-};
+}
+
+Switch::Switch() {}
+
+Switch::~Switch() {}
+
+NodeType Switch::getType() {
+  return COMPUTER;
+}
 
 
 
