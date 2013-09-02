c4f682fd6d41273985ed78b6cef7be2cbc35a12f
Chinmayee Shah <chshah@stanford.edu>
2013-09-01 21:09:27 -0700

    adding deserialization code for face arrays

---
+++ application/water_test_multiple_v1/proto_files/physbam_deserialize_data_common_2d.cc
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2013 Stanford University.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * - Neither the name of the copyright holders nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* 
+ * Author: Chinmayee Shah <chinmayee.shah@stanford.edu>
+ */
+
+#include "assert.h"
+#include "physbam_deserialize_data_common_2d.h"
+
+namespace physbam_pb {
+
+    void make_physbam_object(VI2 *phys_vec,
+            const ::communication::PhysbamVectorInt2d &pb_vec) {
+        assert(phys_vec);
+        if (pb_vec.has_x())
+            (*phys_vec)[1] = pb_vec.x();
+        if (pb_vec.has_y())
+            (*phys_vec)[2] = pb_vec.y();
+    }
+
+    void make_physbam_object(VF2 *phys_vec,
+            const ::communication::PhysbamVectorFloat2d &pb_vec) {
+        assert(phys_vec);
+        if (pb_vec.has_x())
+            (*phys_vec)[1] = pb_vec.x();
+        if (pb_vec.has_y())
+            (*phys_vec)[2] = pb_vec.y();
+    }
+
+    void make_physbam_object(RangeI2 *phys_range,
+            const ::communication::PhysbamRangeInt2d &pb_range) {
+        assert(phys_range);
+        if (pb_range.has_min_corner())
+            make_physbam_object(
+                    &phys_range->min_corner,
+                    pb_range.min_corner());
+        if (pb_range.has_max_corner())
+            make_physbam_object(
+                    &phys_range->max_corner,
+                    pb_range.max_corner());
+    }
+
+    void make_physbam_object(RangeF2 *phys_range,
+            const ::communication::PhysbamRangeFloat2d &pb_range) {
+        assert(phys_range);
+        if (pb_range.has_min_corner())
+            make_physbam_object(
+                    &phys_range->min_corner,
+                    pb_range.min_corner());
+        if (pb_range.has_max_corner())
+            make_physbam_object(
+                    &phys_range->max_corner,
+                    pb_range.max_corner());
+    }
+
+    void make_physbam_object(Grid2 *phys_grid,
+            const ::communication::PhysbamGrid2d &pb_grid) {
+        assert(phys_grid);
+        if (pb_grid.has_counts())
+            make_physbam_object(&phys_grid->counts, pb_grid.counts());
+        if (pb_grid.has_domain())
+            make_physbam_object(&phys_grid->domain, pb_grid.domain());
+        if (pb_grid.has_dx())
+            make_physbam_object(&phys_grid->dX, pb_grid.dx());
+        if (pb_grid.has_one_over_dx())
+            make_physbam_object(
+                    &phys_grid->one_over_dX,
+                    pb_grid.one_over_dx());
+        if (pb_grid.has_numbers_of_cells())
+            make_physbam_object(
+                    &phys_grid->numbers_of_cells,
+                    pb_grid.numbers_of_cells());
+        if (pb_grid.has_min_dx())
+            phys_grid->min_dX = pb_grid.min_dx();
+        if (pb_grid.has_mac_offset())
+            phys_grid->MAC_offset = pb_grid.mac_offset();
+    }
+
+} // namespace physbam_pb
