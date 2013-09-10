6e47360fbcdb48fe6cec9f4c8bfc784b8b7bad54
Omid Mashayekhi <omidm@stanford.edu>
2013-09-10 13:17:16 -0700

    statrted to add our first multi worker aplication. goal is to check te data exchange. also have to add the very first scheduler that runs the application over multiple worlers and check its functionality.

---
+++ application/stencil_1d_multi/app.cc
@@ -0,0 +1,380 @@
+/*
+ * Copyright 2013 Stanford University.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * - Neither the name of the copyright holders nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * An Example application that is meant to run over multiple workers.
+ * It is simply applying a stencil over a one dimensional array.
+ *
+ * Author: Omid Mashayekhi<omidm@stanford.edu>
+ */
+
+#include "./app.h"
+
+Vec::Vec(int size) {
+  this->size = size;
+};
+
+Data * Vec::Clone() {
+  std::cout << "Cloning Vec data!\n";
+  return new Vec(size);
+};
+
+void Vec::Create() {
+  arr = new int[size];
+};
+
+App::App() {
+};
+
+void App::Load() {
+  std::cout << "Start Creating Data and Job Tables" << std::endl;
+
+  RegisterJob("main", new Main(this));
+  RegisterJob("init", new Init());
+  RegisterJob("forLoop", new ForLoop(this));
+  RegisterJob("print", new Print());
+  RegisterJob("applyLeft", new ApplyLeft());
+  RegisterJob("applyRight", new ApplyRight());
+
+  RegisterData("middle", new Vec(ML - 1));
+  RegisterData("side", new Vec(GL));
+
+  std::cout << "Finished Creating Data and Job Tables" << std::endl;
+};
+
+Main::Main(Application* app) {
+  set_application(app);
+};
+
+Job * Main::Clone() {
+  std::cout << "Cloning main job!\n";
+  return new Main(application());
+};
+
+void Main::Execute(std::string params, const DataArray& da) {
+  std::cout << "Executing the main job\n";
+  std::vector<job_id_t> j;
+  std::vector<data_id_t> d;
+  IDSet<data_id_t> read, write;
+  IDSet<job_id_t> before, after;
+  IDSet<partition_t> neighbor_partitions;
+  partition_t p_1 = 1;
+  partition_t p_2 = 2;
+  std::string par;
+
+  GetNewJobID(&j, 7);
+  GetNewDataID(&d, 8);
+
+  DefineData("side", d[0], p_1, neighbor_partitions, par);
+  DefineData("middle", d[1], p_1, neighbor_partitions, par);
+  DefineData("side", d[2], p_1, neighbor_partitions, par);
+  DefineData("side", d[3], p_2, neighbor_partitions, par);
+  DefineData("middle", d[4], p_2, neighbor_partitions, par);
+  DefineData("side", d[5], p_2, neighbor_partitions, par);
+  DefineData("side", d[6], p_2, neighbor_partitions, par);
+  DefineData("side", d[7], p_1, neighbor_partitions, par);
+
+  read.clear(); read.insert(d[0]);
+  write.clear(); write.insert(d[0]);
+  before.clear();
+  after.clear(); after.insert(j[6]);
+  par = ID<data_id_t>(0).toString();
+  SpawnComputeJob("init", j[0], read, write, before, after, par);
+
+  read.clear(); read.insert(d[1]);
+  write.clear(); write.insert(d[1]);
+  before.clear();
+  after.clear(); after.insert(j[6]);
+  par = ID<data_id_t>(0).toString();
+  SpawnComputeJob("init", j[1], read, write, before, after, par);
+
+  read.clear(); read.insert(d[2]);
+  write.clear(); write.insert(d[2]);
+  before.clear();
+  after.clear(); after.insert(j[6]);
+  par = ID<data_id_t>(ML - 1).toString();
+  SpawnComputeJob("init", j[2], read, write, before, after, par);
+
+  read.clear(); read.insert(d[3]);
+  write.clear(); write.insert(d[3]);
+  before.clear();
+  after.clear(); after.insert(j[6]);
+  par = ID<data_id_t>(0).toString();
+  SpawnComputeJob("init", j[3], read, write, before, after, par);
+
+  read.clear(); read.insert(d[4]);
+  write.clear(); write.insert(d[4]);
+  before.clear();
+  after.clear(); after.insert(j[6]);
+  par = ID<data_id_t>(1).toString();
+  SpawnComputeJob("init", j[4], read, write, before, after, par);
+
+  read.clear(); read.insert(d[5]);
+  write.clear(); write.insert(d[5]);
+  before.clear();
+  after.clear(); after.insert(j[6]);
+  par = ID<data_id_t>(0).toString();
+  SpawnComputeJob("init", j[5], read, write, before, after, par);
+
+  read.clear();
+  write.clear();
+  before.clear(); before.insert(j[0]); before.insert(j[1]); before.insert(j[2]);
+  before.insert(j[3]); before.insert(j[4]); before.insert(j[5]);
+  after.clear();
+  IDSet<data_id_t> temp_set;
+  temp_set.insert(d[0]); temp_set.insert(d[1]); temp_set.insert(d[2]); temp_set.insert(d[3]);
+  temp_set.insert(d[4]); temp_set.insert(d[5]); temp_set.insert(d[6]); temp_set.insert(d[7]);
+  par = temp_set.toString();
+  par += ("-" + ID<data_id_t>(LOOP_COUNTER).toString());
+  SpawnComputeJob("forLoop", j[6], read, write, before, after, par);
+};
+
+ForLoop::ForLoop(Application* app) {
+  set_application(app);
+};
+
+Job * ForLoop::Clone() {
+  std::cout << "Cloning forLoop job!\n";
+  return new ForLoop(application());
+};
+
+void ForLoop::Execute(std::string params, const DataArray& da) {
+  std::cout << "Executing the forLoop job\n";
+  std::vector<job_id_t> j;
+  std::vector<data_id_t> d;
+  IDSet<data_id_t> read, write;
+  IDSet<job_id_t> before, after;
+  std::string par;
+
+  char_separator<char> separator("-");
+  tokenizer<char_separator<char> > tokens(params, separator);
+  tokenizer<char_separator<char> >::iterator iter = tokens.begin();
+
+  std::set<data_id_t> temp_set;
+  nimbus::ParseIDSet(*iter, temp_set);
+  std::set<data_id_t>::iterator it;
+  for (it = temp_set.begin(); it != temp_set.end(); it++) {
+    d.push_back(*it);
+  }
+
+  iter++;
+  uint32_t counter;
+  nimbus::ParseID(*iter, counter);
+
+//  d.push_back(16777217);
+//  d.push_back(16777218);
+//  d.push_back(16777219);
+//  d.push_back(16777220);
+
+
+  if (counter > LOOP_CONDITION) {
+    GetNewJobID(&j, 5);
+
+    before.clear();
+    after.clear(); after.insert(j[2]); after.insert(j[3]);
+    SpawnCopyJob(j[0], d[2], d[6], before, after, par);
+
+    before.clear();
+    after.clear(); after.insert(j[2]); after.insert(j[3]);
+    SpawnCopyJob(j[1], d[3], d[7], before, after, par);
+
+    read.clear(); read.insert(d[0]); read.insert(d[1]); read.insert(d[2]); read.insert(d[7]);
+    write.clear(); write.insert(d[1]); write.insert(d[2]);
+    before.clear(); before.insert(j[0]); before.insert(j[1]);
+    after.clear(); after.insert(j[4]);
+    SpawnComputeJob("applyLeft", j[2], read, write, before, after, par);
+
+    read.clear(); read.insert(d[3]); read.insert(d[4]); read.insert(d[5]); read.insert(d[6]);
+    write.clear(); write.insert(d[3]); write.insert(d[4]);
+    before.clear(); before.insert(j[0]); before.insert(j[1]);
+    after.clear(); after.insert(j[4]);
+    SpawnComputeJob("applyRight", j[3], read, write, before, after, par);
+
+    read.clear();
+    write.clear();
+    before.clear(); before.insert(j[2]); before.insert(j[3]);
+    after.clear();
+    IDSet<data_id_t> temp_set;
+    temp_set.insert(d[0]); temp_set.insert(d[1]); temp_set.insert(d[2]); temp_set.insert(d[3]);
+    par = temp_set.toString();
+    par += ("-" + ID<data_id_t>(counter - 1).toString());
+    SpawnComputeJob("forLoop", j[4], read, write, before, after, par);
+  } else {
+    GetNewJobID(&j, 2);
+
+    read.clear(); read.insert(d[1]); read.insert(d[2]);
+    write.clear();
+    before.clear();
+    after.clear();
+    SpawnComputeJob("print", j[0], read, write, before, after, par);
+
+    read.clear(); read.insert(d[3]); read.insert(d[4]);
+    write.clear();
+    before.clear();
+    after.clear();
+    SpawnComputeJob("print", j[1], read, write, before, after, par);
+  }
+};
+
+Init::Init() {
+};
+
+Job * Init::Clone() {
+  std::cout << "Cloning init job!\n";
+  return new Init();
+};
+
+void Init::Execute(std::string params, const DataArray& da) {
+  std::cout << "Executing the init job\n";
+  Vec *d = reinterpret_cast<Vec*>(da[0]);
+  for (int i = 0; i < d->size ; i++)
+    d->arr[i] = i;
+};
+
+
+Print::Print() {
+};
+
+Job * Print::Clone() {
+  std::cout << "Cloning print job!\n";
+  return new Print();
+};
+
+void Print::Execute(std::string params, const DataArray& da) {
+  std::cout << "Executing the print job\n";
+  Vec *d = reinterpret_cast<Vec*>(da[0]);
+  for (int i = 0; i < d->size; i++)
+    std::cout << d->arr[i] << ", ";
+  std::cout << std::endl;
+};
+
+
+ApplyLeft::ApplyLeft() {
+};
+
+Job * ApplyLeft::Clone() {
+  std::cout << "Cloning applyLeft job!\n";
+  return new ApplyLeft();
+};
+
+void ApplyLeft::Execute(std::string params, const DataArray& da) {
+  std::cout << "Executing the applyLeft job\n";
+  int sten[] = {-1, +2, -1, 0};
+
+  Vec *d1 = reinterpret_cast<Vec*>(da[0]);
+  Vec *d2 = reinterpret_cast<Vec*>(da[1]);
+
+  int len = d1->size;
+  int *main = d1->arr;
+  int *ghost = d2->arr;
+  int temp[len]; // NOLINT
+
+  temp[0] = sten[0] * sten[3] + sten[1] * main[0] + sten[2] * main[1];
+  temp[len-1] = sten[0] * main[len-2] + sten[1] * main[len-1] + sten[2] * ghost[0];
+  for (int i = 1; i < (len - 1); i++)
+    temp[i] = sten[0] * main[i-1] + sten[1] * main[i] + sten[2] * main[i+1];
+
+  for (int i = 0; i < len; i++)
+    main[i] = temp[i];
+};
+
+ApplyRight::ApplyRight() {
+};
+
+Job * ApplyRight::Clone() {
+  std::cout << "Cloning applyRight job!\n";
+  return new ApplyRight();
+};
+
+void ApplyRight::Execute(std::string params, const DataArray& da) {
+  std::cout << "Executing the applyRight job\n";
+  int sten[] = {-1, +2, -1, 0};
+
+  Vec *d1 = reinterpret_cast<Vec*>(da[0]);
+  Vec *d2 = reinterpret_cast<Vec*>(da[1]);
+
+  int len = d1->size;
+  int *main = d1->arr;
+  int *ghost = d2->arr;
+  int temp[len]; // NOLINT
+
+  temp[0] = sten[0] * ghost[0] + sten[1] * main[0] + sten[2] * main[1];
+  temp[len-1] = sten[0] * main[len-2] + sten[1] * main[len-1] + sten[2] * sten[3];
+  for (int i = 1; i < (len - 1); i++)
+    temp[i] = sten[0] * main[i-1] + sten[1] * main[i] + sten[2] * main[i+1];
+
+  for (int i = 0; i < len; i++)
+    main[i] = temp[i];
+};
+
+UpdateLeft::UpdateLeft() {
+};
+
+Job * UpdateLeft::Clone() {
+  std::cout << "Cloning updateLeft job!\n";
+  return new UpdateLeft();
+};
+
+void UpdateLeft::Execute(std::string params, const DataArray& da) {
+  std::cout << "Executing the updateLeft job\n";
+  Vec *d1 = reinterpret_cast<Vec*>(da[0]);
+  Vec *d2 = reinterpret_cast<Vec*>(da[1]);
+
+  int *main = d1->arr;
+  int *ghost = d2->arr;
+  ghost[0] = main[0];
+};
+
+UpdateRight::UpdateRight() {
+};
+
+Job * UpdateRight::Clone() {
+  std::cout << "Cloning updateRight job!\n";
+  return new UpdateRight();
+};
+
+void UpdateRight::Execute(std::string params, const DataArray& da) {
+  std::cout << "Executing the updateRight job\n";
+  Vec *d1 = reinterpret_cast<Vec*>(da[0]);
+  Vec *d2 = reinterpret_cast<Vec*>(da[1]);
+
+  int *main = d1->arr;
+  int *ghost = d2->arr;
+  ghost[0] = main[d1->size - 1];
+};
+
+
+
+
+
---
+++ application/stencil_1d_multi/app.h
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2013 Stanford University.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * - Neither the name of the copyright holders nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * An Example application that is meant to run over multiple workers.
+ * It is simply applying a stencil over a one dimensional array.
+ *
+ * Author: Omid Mashayekhi<omidm@stanford.edu>
+ */
+
+#ifndef NIMBUS_APPLICATION_STENCIL_1D_MULTI_APP_H_
+#define NIMBUS_APPLICATION_STENCIL_1D_MULTI_APP_H_
+
+#include <boost/tokenizer.hpp>
+#include <string>
+#include <iostream> // NOLINT
+#include "worker/application.h"
+#include "worker/job.h"
+#include "worker/data.h"
+#include "shared/parser.h"
+#include "shared/nimbus_types.h"
+
+#define ML 4
+#define GL 1
+#define LOOP_COUNTER 3
+#define LOOP_CONDITION 0
+
+using nimbus::Job;
+using nimbus::Data;
+using nimbus::Application;
+using boost::tokenizer;
+using boost::char_separator;
+
+
+class Main : public Job {
+  public:
+    explicit Main(Application* app);
+    virtual void Execute(std::string params, const DataArray& da);
+    virtual Job * Clone();
+};
+
+class Init : public Job {
+  public:
+    Init();
+    virtual void Execute(std::string params, const DataArray& da);
+    virtual Job * Clone();
+};
+
+class Print : public Job {
+  public:
+    Print();
+    virtual void Execute(std::string params, const DataArray& da);
+    virtual Job * Clone();
+};
+
+class ApplyLeft : public Job {
+  public:
+    ApplyLeft();
+    virtual void Execute(std::string params, const DataArray& da);
+    virtual Job * Clone();
+};
+
+class ApplyRight : public Job {
+  public:
+    ApplyRight();
+    virtual void Execute(std::string params, const DataArray& da);
+    virtual Job * Clone();
+};
+
+class UpdateLeft : public Job {
+  public:
+    UpdateLeft();
+    virtual void Execute(std::string params, const DataArray& da);
+    virtual Job * Clone();
+};
+
+class UpdateRight : public Job {
+  public:
+    UpdateRight();
+    virtual void Execute(std::string params, const DataArray& da);
+    virtual Job * Clone();
+};
+
+class ForLoop : public Job {
+  public:
+    explicit ForLoop(Application* app);
+    virtual void Execute(std::string params, const DataArray& da);
+    virtual Job * Clone();
+};
+
+class Vec : public Data {
+  public:
+    explicit Vec(int size);
+    int size;
+    int *arr;
+    virtual void Create();
+    virtual Data * Clone();
+};
+
+class App : public Application {
+  public:
+    App();
+    virtual void Load();
+};
+
+
+
+
+
+
+
+
+
+
+
+#endif  // NIMBUS_APPLICATION_STENCIL_1D_MULTI_APP_H_
