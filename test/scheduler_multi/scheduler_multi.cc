96ef2fa88df656ac4eb42cb587ced38d9eb0a6ea
Omid Mashayekhi <omidm@stanford.edu>
2013-09-10 15:15:46 -0700

    started to write the very first scheduler that runs the application over multiple workers.

---
+++ test/scheduler_multi/scheduler_multi.cc
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2013 Stanford University.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * - Neither the name of the copyright holders nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+ /*
+  * Simple Nimbus scheduler that is supposed to run the application over
+  * multiple workers. This scheduler is not still smart enough to make the
+  * dynamic decisions. It assigned each part of the simulation to a worker.
+  * This test is intended to check the data exchange functionality and how the
+  * system correctly enforces the correct program frlow.
+  *
+  * Author: Omid Mashayekhi <omidm@stanford.edu>
+  */
+
+#include "./scheduler_multi.h"
+
+SimpleScheduler::SimpleScheduler(unsigned int p)
+: Scheduler(p) {
+}
+
+void SimpleScheduler::SchedulerCoreProcessor() {
+  Log::dbg_printLine("Simple Scheduler Core Processor");
+
+  while (true) {
+    sleep(1);
+    int ready_num = 0;
+    SchedulerWorkerList::iterator iter;
+    for (iter = server_->workers()->begin();
+        iter != server_->workers()->end(); iter++) {
+      if ((*iter)->handshake_done()) {
+        ready_num++;
+      } else {
+        ID<worker_id_t> worker_id;
+        worker_id.set_elem((*iter)->worker_id());
+        std::string ip("you-know");
+        ID<port_t> port(0);
+        HandshakeCommand cm(worker_id, ip, port);
+        std::cout << "Sending command: " << cm.toStringWTags() << std::endl;
+        server_->SendCommand(*iter, &cm);
+      }
+    }
+    if (ready_num >= WORKER_NUM)
+      break;
+
+    std::cout << ready_num << " worker(s) are registered, waiting for " <<
+      WORKER_NUM - ready_num << " more worker(s) to join ..."  << std::endl;
+
+    SchedulerCommandList storage;
+    if (server_->ReceiveCommands(&storage, (uint32_t)10)) {
+      SchedulerCommandList::iterator iter = storage.begin();
+      for (; iter != storage.end(); iter++) {
+        SchedulerCommand* comm = *iter;
+        std::cout << "Received command: " << comm->toStringWTags() << std::endl;
+        ProcessSchedulerCommand(comm);
+        delete comm;
+      }
+    }
+  }
+
+
+  std::vector<job_id_t> j;
+  id_maker_.GetNewJobID(&j, 1);
+  ID<job_id_t> id(j[0]);
+  IDSet<data_id_t> read, write;
+  IDSet<job_id_t> before, after;
+  std::string params;
+  ComputeJobCommand cm("main", id, read, write, before, after, params);
+  std::cout << "Sending command: " << cm.toStringWTags() << std::endl;
+  server_->SendCommand(*(server_->workers()->begin()), &cm);
+
+  while (true) {
+    SchedulerCommandList storage;
+    if (server_->ReceiveCommands(&storage, (uint32_t)10)) {
+      SchedulerCommandList::iterator iter = storage.begin();
+      for (; iter != storage.end(); iter++) {
+        SchedulerCommand* comm = *iter;
+        std::cout << "Received command: " << comm->toStringWTags() << std::endl;
+        ProcessSchedulerCommand(comm);
+        delete comm;
+      }
+    }
+
+    SchedulerCommandList::iterator iter;
+    for (iter = pending_compute_jobs_.begin();
+        iter != pending_compute_jobs_.end();) {
+      ComputeJobCommand* comm = reinterpret_cast<ComputeJobCommand*>(*iter);
+      bool data_created = true;
+      IDSet<data_id_t>::IDSetIter it;
+      IDSet<data_id_t> read = comm->read_set();
+      for (it = read.begin(); it != read.end(); it++) {
+        if (create_data_.count(*it) == 0) {
+          data_created = false;
+          break;
+        } else if (!create_data_[*it]) {
+          data_created = false;
+          break;
+        }
+      }
+      IDSet<data_id_t> write = comm->write_set();
+      for (it = write.begin(); it != write.end(); it++) {
+        if (create_data_.count(*it) == 0) {
+          data_created = false;
+          break;
+        } else if (!create_data_[*it]) {
+          data_created = false;
+          break;
+        }
+      }
+      if (data_created) {
+        std::cout << "Sending command: " << comm->toStringWTags() << std::endl;
+        server_->SendCommand(*(server_->workers()->begin()), comm);
+        pending_compute_jobs_.erase(iter++);
+        delete comm;
+      } else {
+        ++iter;
+      }
+      // sleep(1);
+    }
+  }
+}
+
+void SimpleScheduler::ProcessSpawnComputeJobCommand(SpawnComputeJobCommand* cm) {
+  SchedulerCommand* comm = new ComputeJobCommand(cm->job_name(), cm->job_id(),
+      cm->read_set(), cm->write_set(), cm->before_set(), cm->after_set(),
+      cm->params());
+  pending_compute_jobs_.push_back(comm);
+}
+
+void SimpleScheduler::ProcessDefineDataCommand(DefineDataCommand* cm) {
+  std::vector<job_id_t> j;
+  id_maker_.GetNewJobID(&j, 1);
+  ID<job_id_t> id(j[0]);
+  IDSet<job_id_t> before, after;
+  SchedulerCommand* comm = new CreateDataCommand(id, cm->data_name(),
+      cm->data_id(), before, after);
+  job_data_map_[id.elem()] = cm->data_id().elem();
+  create_data_[cm->data_id().elem()] = false;
+  std::cout << "Sending command: " << comm->toStringWTags() << std::endl;
+  server_->SendCommand(*(server_->workers()->begin()), comm);
+  delete comm;
+}
+
+void SimpleScheduler::ProcessJobDoneCommand(JobDoneCommand* cm) {
+  SchedulerWorkerList::iterator iter = server_->workers()->begin();
+  for (; iter != server_->workers()->end(); iter++) {
+    std::cout << "Sending command: " << cm->toStringWTags() << std::endl;
+    server_->SendCommand(*iter, cm);
+  }
+  if (job_data_map_.count(cm->job_id().elem()) != 0) {
+    create_data_[job_data_map_[cm->job_id().elem()]] = true;
+  }
+}
+
+
+
---
+++ test/scheduler_multi/scheduler_multi.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2013 Stanford University.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * - Neither the name of the copyright holders nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+ /*
+  * Simple Nimbus scheduler that is supposed to run the application over
+  * multiple workers. This scheduler is not still smart enough to make the
+  * dynamic decisions. It assigned each part of the simulation to a worker.
+  * This test is intended to check the data exchange functionality and how the
+  * system correctly enforces the correct program frlow.
+  *
+  * Author: Omid Mashayekhi <omidm@stanford.edu>
+  */
+
+#ifndef NIMBUS_TEST_SCHEDULER_MULTI_SCHEDULER_MULTI_H_
+#define NIMBUS_TEST_SCHEDULER_MULTI_SCHEDULER_MULTI_H_
+
+#define DEBUG_MODE
+
+#include <boost/thread.hpp>
+#include <iostream> // NOLINT
+#include <fstream> // NOLINT
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include "shared/dbg.h"
+#include "shared/nimbus.h"
+#include "shared/scheduler_server.h"
+#include "shared/cluster.h"
+#include "shared/parser.h"
+#include "scheduler/scheduler.h"
+
+#define WORKER_NUM 2
+
+class SimpleScheduler : public Scheduler {
+  public:
+    explicit SimpleScheduler(unsigned int listening_port);
+
+    virtual  void SchedulerCoreProcessor();
+    virtual void ProcessSpawnComputeJobCommand(SpawnComputeJobCommand* cm);
+    virtual void ProcessDefineDataCommand(DefineDataCommand* cm);
+    virtual void ProcessJobDoneCommand(JobDoneCommand* cm);
+
+  private:
+    SchedulerCommandList pending_compute_jobs_;
+    std::map<job_id_t, data_id_t> job_data_map_;
+    std::map<data_id_t, bool> create_data_;
+};
+
+#endif  // NIMBUS_TEST_SCHEDULER_MULTI_SCHEDULER_MULTI_H_
