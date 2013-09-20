bc0905657d3195c95f50a77099eb30dbde9eb85f
Zhihao Jia <zhihao@stanford.edu>
2013-09-19 17:58:54 -0700

    fix tiny bug in src/nimbus/application/water_test_multiple_v1/proto_files/clean_proto.sh

--- application/water_test_multiple_v1/proto_files/clean_proto.sh
+++ application/water_test_multiple_v1/proto_files/clean_proto.sh
@@ -4,4 +4,4 @@ rm -rf physbam_vector_2d.pb.h physbam_vector_2d.pb.cc
 rm -rf physbam_range_2d.pb.h physbam_range_2d.pb.cc
 rm -rf physbam_grid_2d.pb.h physbam_grid_2d.pb.cc
 rm -rf physbam_face_array_2d.pb.h physbam_face_array_2d.pb.cc
-rm -rf app_face_array_2d.pb.h app_face_array_ap_2d.pb.cc
+rm -rf app_face_array_2d.pb.h app_face_array_2d.pb.cc
