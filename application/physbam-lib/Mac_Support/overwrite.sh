0a7475057e61acdab8519b9de6aa6be950f1ebd5
Hang Qu <quhang@stanford.edu>
2013-12-05 16:22:01 -0800

    Instructions on how to build PhysBAM on OS X.

---
+++ application/physbam-lib/Mac_Support/overwrite.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp main.cpp ../../physbam-app/opengl_2d/main.cpp
+cp CMakeLists.txt ../Public_Library/CMakeLists.txt
+cp OPENGL_AGL_PBUFFER.cpp ../Public_Library/PhysBAM_Rendering/PhysBAM_OpenGL/OpenGL/OPENGL_AGL_PBUFFER.cpp
+cp PHOTON_MAP.cpp ../Public_Library/PhysBAM_Rendering/PhysBAM_Ray_Tracing/Rendering/PHOTON_MAP.cpp
+cp THREADED_RIGIDS.h ../Public_Library/PhysBAM_Solids/PhysBAM_Rigids/Parallel_Computation/THREADED_RIGIDS.h
+cp Is_NaN.h ../Public_Library/PhysBAM_Tools/Math_Tools/Is_NaN.h
+cp THREAD_QUEUE.cpp ../Public_Library/PhysBAM_Tools/Parallel_Computation/THREAD_QUEUE.cpp
+cp Public_Library.cmake ../Scripts/CMake/Public_Library.cmake
