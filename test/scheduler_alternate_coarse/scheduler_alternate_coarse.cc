a92f788ad5fe30f97ce2d1e06b14f1f8d6136e4c
Omid Mashayekhi <omidm@stanford.edu>
2013-12-23 15:20:14 -0800

    Started the scheduler for water_alternate_coarse application.  Need to update the base scheduler class to remove/reuse the obsolete physical data objects. Right now, we do not take care of them and for alternate_coarse it will work as if we have memory leak.  It has been fine since up to now the only create/copy pair was called for ghost cells, but now the whole state (WATER_EXAMPLE, etc.) needs to be exchanged and the cost of memory leak is not acceptable (As Chinmayee faced the issue of system call time out, we may not be able to compute more than a couple of frames).
    After finishing and testing the memory leak issue, will work on the
    semantics to enforce alternation between workers.

---
+++ test/scheduler_alternate_coarse/scheduler_alternate_coarse.cc
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2013 Stanford University.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * - Neither the name of the copyright holders nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+ /*
+  * This scheduler is written to alternate the iterations for each frame of
+  * water simulation between two workers. It is guaranteed that after
+  * convergence for each frame the write_frame job will be executed over the
+  * same worker, so that all the output frames are local to one worker for
+  * sanity checks.
+  *
+  * Author: Omid Mashayekhi <omidm@stanford.edu>
+  */
+
+#include "./scheduler_alternate_coarse.h"
+#define WORKER_NUM 2
+
+SchedulerAlternateCoarse::SchedulerAlternateCoarse(unsigned int p)
+: Scheduler(p) {
+}
+
+bool SchedulerAlternateCoarse::GetWorkerToAssignJob(JobEntry* job, SchedulerWorker*& worker) {
+  return false;
+}
---
+++ test/scheduler_alternate_coarse/scheduler_alternate_coarse.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2013 Stanford University.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * - Neither the name of the copyright holders nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+ /*
+  * This scheduler is written to alternate the iterations for each frame of
+  * water simulation between two workers. It is guaranteed that after
+  * convergence for each frame the write_frame job will be executed over the
+  * same worker, so that all the output frames are local to one worker for
+  * sanity checks.
+  *
+  * Author: Omid Mashayekhi <omidm@stanford.edu>
+  */
+
+#ifndef NIMBUS_TEST_SCHEDULER_ALTERNATE_COARSE_SCHEDULER_ALTERNATE_COARSE_H_
+#define NIMBUS_TEST_SCHEDULER_ALTERNATE_COARSE_SCHEDULER_ALTERNATE_COARSE_H_
+
+#define DEBUG_MODE
+
+#include <boost/thread.hpp>
+#include <iostream> // NOLINT
+#include <fstream> // NOLINT
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include "shared/dbg.h"
+#include "shared/nimbus.h"
+#include "shared/scheduler_server.h"
+#include "shared/cluster.h"
+#include "shared/parser.h"
+#include "scheduler/scheduler.h"
+
+class SchedulerAlternateCoarse : public Scheduler {
+  public:
+    explicit SchedulerAlternateCoarse(unsigned int listening_port);
+
+    virtual bool GetWorkerToAssignJob(JobEntry* job, SchedulerWorker*& worker);
+};
+
+#endif  // NIMBUS_TEST_SCHEDULER_ALTERNATE_COARSE_SCHEDULER_ALTERNATE_COARSE_H_
