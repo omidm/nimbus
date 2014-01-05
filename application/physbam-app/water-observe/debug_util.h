2f4b7325633ec7f3309e8fe9a99a212321db9520
Hang Qu(sing-pc) <quhang@stanford.edu>
2014-01-05 15:06:17 -0800

    Change from printf to dbg printing utility.

--- application/physbam-app/water-observe/debug_util.h
+++ application/physbam-app/water-observe/debug_util.h
@@ -2,10 +2,15 @@
 
 /*
  * Utilities to print out information for debug.
+ * Can work with normal PhysBAM simulations.
  * Author: Hang Qu <quhang@stanford.edu>
  */
 #ifndef DEBUG_UTIL_H_
 #define DEBUG_UTIL_H_
+
+// If you want to work with Nimbus, please comment out the following two lines.
+#define DBG_WARN 1
+#define dbg(X, ...) printf(__VA_ARGS__)
 
 namespace PhysBAM {
 
@@ -13,35 +18,35 @@ template <typename T_GRID>
 void PrintGrid(const T_GRID& grid) {
   if (grid.dimension == 2) {
     if (grid.Is_MAC_Grid()) {
-      printf("MAC grid{");
+      dbg(DBG_WARN, "MAC grid{");
     } else {
-      printf("Normal grid{ ");
+      dbg(DBG_WARN, "Normal grid{ ");
     }
-    printf("count=(%d,%d);",
+    dbg(DBG_WARN, "count=(%d,%d);",
            grid.counts(1),
            grid.counts(2));
-    printf("min=(%.1f,%.1f);",
+    dbg(DBG_WARN, "min=(%.1f,%.1f);",
            (float)grid.domain.min_corner(1),
            (float)grid.domain.min_corner(2));
-    printf("max=(%.1f,%.1f) }\n",
+    dbg(DBG_WARN, "max=(%.1f,%.1f) }\n",
            (float)grid.domain.max_corner(1),
            (float)grid.domain.max_corner(2));
   }
   if (grid.dimension == 3) {
     if (grid.Is_MAC_Grid()) {
-      printf("MAC grid{");
+      dbg(DBG_WARN, "MAC grid{");
     } else {
-      printf("Normal grid{ ");
+      dbg(DBG_WARN, "Normal grid{ ");
     }
-    printf("count=(%d,%d,%d);",
+    dbg(DBG_WARN, "count=(%d,%d,%d);",
            grid.counts(1),
            grid.counts(2),
            grid.counts(3));
-    printf("min=(%.1f,%.1f,%.1f);",
+    dbg(DBG_WARN, "min=(%.1f,%.1f,%.1f);",
            (float)grid.domain.min_corner(1),
            (float)grid.domain.min_corner(2),
            (float)grid.domain.min_corner(3));
-    printf("max=(%.1f,%.1f,%.1f) }\n",
+    dbg(DBG_WARN, "max=(%.1f,%.1f,%.1f) }\n",
            (float)grid.domain.max_corner(1),
            (float)grid.domain.max_corner(2),
            (float)grid.domain.max_corner(3));
@@ -51,7 +56,7 @@ void PrintGrid(const T_GRID& grid) {
 template<typename T_GRID>
 void PrintParticles(const PARTICLE_LEVELSET_UNIFORM<T_GRID>& container,
                     bool detail = false) {
-  printf("Dump info of particles:\n");
+  dbg(DBG_WARN, "Dump info of particles:\n");
   PrintGrid(container.levelset.grid);
   PrintArrayProfile(container.negative_particles);
   if (detail) {
@@ -69,11 +74,11 @@ void PrintParticles(const PARTICLE_LEVELSET_UNIFORM<T_GRID>& container,
         for(int k = 1; k <= cell_particles->array_collection->Size(); k++) {
           assert(cell_particles->radius(k) > 0);
           if (T_GRID::VECTOR_INT::dimension == 2) {
-            printf("X(%.1f,%.1f),", cell_particles->X(k)(1),
+            dbg(DBG_WARN, "X(%.1f,%.1f),", cell_particles->X(k)(1),
                    cell_particles->X(k)(2));
           }
           if (T_GRID::VECTOR_INT::dimension == 3) {
-            printf("X(%.1f,%.1f,%.1f),", cell_particles->X(k)(1),
+            dbg(DBG_WARN, "X(%.1f,%.1f,%.1f),", cell_particles->X(k)(1),
                    cell_particles->X(k)(2), cell_particles->X(k)(3));
           }
         }
@@ -82,32 +87,32 @@ void PrintParticles(const PARTICLE_LEVELSET_UNIFORM<T_GRID>& container,
       }
     }
   }
-  printf("\n[End] Dump info of particles:\n");
+  dbg(DBG_WARN, "\n[End] Dump info of particles:\n");
 }
 
 template <typename T, int d>
 void PrintArrayProfile(const ARRAY< T,VECTOR<int,d> >& array) {
   if (d == 2) {
-    printf("count=(%d,%d);",
+    dbg(DBG_WARN, "count=(%d,%d);",
            array.counts(1),
            array.counts(2));
-    printf("min=(%.1f,%.1f);",
+    dbg(DBG_WARN, "min=(%.1f,%.1f);",
            (float)array.domain.min_corner(1),
            (float)array.domain.min_corner(2));
-    printf("max=(%.1f,%.1f) }\n",
+    dbg(DBG_WARN, "max=(%.1f,%.1f) }\n",
            (float)array.domain.max_corner(1),
            (float)array.domain.max_corner(2));
   }
   if (d == 3) {
-    printf("count=(%d,%d,%d);",
+    dbg(DBG_WARN, "count=(%d,%d,%d);",
            array.counts(1),
            array.counts(2),
            array.counts(3));
-    printf("min=(%.1f,%.1f,%.1f);",
+    dbg(DBG_WARN, "min=(%.1f,%.1f,%.1f);",
            (float)array.domain.min_corner(1),
            (float)array.domain.min_corner(2),
            (float)array.domain.min_corner(3));
-    printf("max=(%.1f,%.1f,%.1f) }\n",
+    dbg(DBG_WARN, "max=(%.1f,%.1f,%.1f) }\n",
            (float)array.domain.max_corner(1),
            (float)array.domain.max_corner(2),
            (float)array.domain.max_corner(3));
