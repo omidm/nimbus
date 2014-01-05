7e2aea1c8dcb386f71bec2ec4ccf59d6f607c5b1
Hang Qu(sing-pc) <quhang@stanford.edu>
2014-01-05 14:32:22 -0800

    debug_util.h ready for usage.

--- application/physbam-app/water-observe/debug_util.h
+++ application/physbam-app/water-observe/debug_util.h
@@ -51,26 +51,38 @@ void PrintGrid(const T_GRID& grid) {
 template<typename T_GRID>
 void PrintParticles(const PARTICLE_LEVELSET_UNIFORM<T_GRID>& container,
                     bool detail = false) {
+  printf("Dump info of particles:\n");
   PrintGrid(container.levelset.grid);
   PrintArrayProfile(container.negative_particles);
   if (detail) {
-    RANGE<T_GRID::TV_INT> domain(
+    RANGE<typename T_GRID::VECTOR_INT> domain(
         container.levelset.grid.Domain_Indices(
             container.number_of_ghost_cells));
-    domain.max_corner += T_GRID::TV_INT::All_Ones_Vector();
-    for (T_GRID::NODE_ITERATOR iterator(container.levelset.grid, domain);
+    domain.max_corner += T_GRID::VECTOR_INT::All_Ones_Vector();
+    for (typename T_GRID::NODE_ITERATOR
+         iterator(container.levelset.grid, domain);
          iterator.Valid();iterator.Next()) {
-      T_GRID::TV_INT block=iterator.Node_Index();
-      typename PARTICLE_LEVELSET_UNIFORM<T_GRID>::T_ARRAYS_PARTICLES::ELEMENT
-          cell_particles = particles(block);
+      typename T_GRID::VECTOR_INT block=iterator.Node_Index();
+      PARTICLE_LEVELSET_PARTICLES<typename T_GRID::VECTOR_T>* cell_particles =
+          container.positive_particles(block);
       while (cell_particles) {
-        for(int k = 1; k <= cell_particles->array_collection->Size(); k++)
+        for(int k = 1; k <= cell_particles->array_collection->Size(); k++) {
           assert(cell_particles->radius(k) > 0);
+          if (T_GRID::VECTOR_INT::dimension == 2) {
+            printf("X(%.1f,%.1f),", cell_particles->X(k)(1),
+                   cell_particles->X(k)(2));
+          }
+          if (T_GRID::VECTOR_INT::dimension == 3) {
+            printf("X(%.1f,%.1f,%.1f),", cell_particles->X(k)(1),
+                   cell_particles->X(k)(2), cell_particles->X(k)(3));
+          }
+        }
         // final_block=levelset.grid.Block_Index(cell_particles->X(k),keep_particles_in_ghost_cells?number_of_ghost_cells+1:1);
         cell_particles = cell_particles->next;
       }
     }
   }
+  printf("\n[End] Dump info of particles:\n");
 }
 
 template <typename T, int d>
@@ -86,12 +98,7 @@ void PrintArrayProfile(const ARRAY< T,VECTOR<int,d> >& array) {
            (float)array.domain.max_corner(1),
            (float)array.domain.max_corner(2));
   }
-  if (grid.dimension == 3) {
-    if (grid.Is_MAC_Grid()) {
-      printf("MAC grid{");
-    } else {
-      printf("Normal grid{ ");
-    }
+  if (d == 3) {
     printf("count=(%d,%d,%d);",
            array.counts(1),
            array.counts(2),
