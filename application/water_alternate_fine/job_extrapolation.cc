693cc10ffe69662fe66f9cd05b007051c5881039
Hang Qu(sing-pc) <quhang@stanford.edu>
2014-01-23 15:19:30 -0800

    Sample commit, showing how to tune DataConfig, so that a job will only load/save specified simulation variables into WATER_EXAMPLE. Unspecified varibles will not be translated from/to WATER_EXAMPLE but might be transmitted to a worker for now.

--- application/water_alternate_fine/job_extrapolation.cc
+++ application/water_alternate_fine/job_extrapolation.cc
@@ -77,7 +77,8 @@ void JobExtrapolation::Execute(nimbus::Parameter params,
   PhysBAM::WATER_DRIVER<TV> *driver;
 
   DataConfig data_config;
-  data_config.SetAll();
+  data_config.SetFlag(DataConfig::VELOCITY);
+  data_config.SetFlag(DataConfig::LEVELSET);
   InitializeExampleAndDriver(init_config, data_config,
                              this, da, example, driver);
 
