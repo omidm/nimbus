4847f055bacdf9ed2e6dfed2284bde6e3db7c711
Hang Qu(sing-pc) <quhang@stanford.edu>
2014-01-31 17:49:46 -0800

    Reduce read/write set for job DELETE_PARTICLES and REINCORPORATE_REMOVED_PARTICLES.

--- application/water_alternate_fine/job_delete_particles.cc
+++ application/water_alternate_fine/job_delete_particles.cc
@@ -78,7 +78,12 @@ void JobDeleteParticles::Execute(nimbus::Parameter params, const nimbus::DataArr
 
     init_config.set_boundary_condition = false;
     DataConfig data_config;
-    data_config.SetAll();
+    data_config.SetFlag(DataConfig::VELOCITY_GHOST);
+    data_config.SetFlag(DataConfig::LEVELSET);
+    data_config.SetFlag(DataConfig::POSITIVE_PARTICLE);
+    data_config.SetFlag(DataConfig::NEGATIVE_PARTICLE);
+    data_config.SetFlag(DataConfig::REMOVED_POSITIVE_PARTICLE);
+    data_config.SetFlag(DataConfig::REMOVED_NEGATIVE_PARTICLE);
     InitializeExampleAndDriver(init_config, data_config,
                                this, da, example, driver);
 
