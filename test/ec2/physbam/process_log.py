849956bf7894135dc67f9cae3a7437521fa13a9a
Omid Mashayekhi <omidm@stanford.edu>
2014-04-20 18:00:13 -0700

    sample results ready for physbam.

--- test/ec2/physbam/process_log.py
+++ test/ec2/physbam/process_log.py
@@ -78,51 +78,58 @@ for i in range(1, config.INSTANCE_NUM + 1):
 
 
 
-
-
-
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 from decimal import *
 getcontext().prec = 2
 
 n_groups = config.INSTANCE_NUM
-y_ticks = 3
 index = np.arange(n_groups)
-
-# mean_time = (np.mean(force), np.mean(advect_phi), np.mean(advect_removed_particles))
-# std_time = (np.std(force), np.std(advect_phi), np.std(advect_removed_particles))
-
-mean_time = force_mean_time
-std_time = force_std_time
+y_ticks = 3
+font = FontProperties()
+font.set_family('serif')
 
 fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, sharex=True)
 
-error_config = {'ecolor': '1'}
+for i in np.arange(len(force_std_time)):
+  if (force_mean_time[i] <= force_std_time[i]):
+    force_std_time[i] = force_mean_time[i] - 1
 
 ax0.errorbar(index, force_mean_time, yerr=force_std_time, fmt='o')
-ax0.set_title('Apply Force Job')
+ax0.set_title('Apply Force Job', fontproperties=font)
 # ax0.set_yscale('log')
+# ax0.set_ylabel('log time(ms)', fontproperties=font)
 ax0.locator_params(axis = 'y', nbins = y_ticks)
+ax0.set_ylabel('time(ms)', fontproperties=font)
 
-# y_min = min(force_mean_time) - max(force_std_time)
-# y_max = max(force_mean_time) + max(force_std_time)
-# y_step = (y_max - y_min) / float(y_ticks)
-# ax0.yaxis.set_ticks(np.arange(y_min, y_max, y_step))
+
+for i in np.arange(len(advect_phi_std_time)):
+  if (advect_phi_mean_time[i] <= advect_phi_std_time[i]):
+    advect_phi_std_time[i] = advect_phi_mean_time[i] - 1
 
 ax1.errorbar(index, advect_phi_mean_time, yerr=advect_phi_std_time, fmt='o')
-ax1.set_title('Advect Phi Job')
+ax1.set_title('Advect Phi Job', fontproperties=font)
 # ax1.set_yscale('log')
+# ax1.set_ylabel('log time(ms)', fontproperties=font)
 ax1.locator_params(axis = 'y', nbins = y_ticks)
+ax1.set_ylabel('time(ms)', fontproperties=font)
+
+
+for i in np.arange(len(advect_removed_particles_std_time)):
+  if (advect_removed_particles_mean_time[i] <= advect_removed_particles_std_time[i]):
+    advect_removed_particles_std_time[i] = advect_removed_particles_mean_time[i] - 1
 
 ax2.errorbar(index, advect_removed_particles_mean_time, yerr=advect_removed_particles_std_time, fmt='o')
-ax2.set_title('Advect Removed Particles Job')
-# ax2.set_yscale('log')
-ax2.locator_params(axis = 'y', nbins = y_ticks)
+ax2.set_title('Advect Removed Particles Job', fontproperties=font)
+ax2.set_yscale('log')
+ax2.set_ylabel('log time(ms)', fontproperties=font)
+# ax2.locator_params(axis = 'y', nbins = y_ticks)
+# ax2.set_ylabel('time(ms)', fontproperties=font)
 
 plt.xlim((-1, n_groups))
-plt.xlabel('Partition')
-plt.xticks(index, ('I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII'))
+plt.xlabel('Partition', fontproperties=font)
+plt.xticks(index, ('I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII'), fontproperties=font)
 
 # plt.grid()
 plt.savefig("test.png")
@@ -132,42 +139,6 @@ plt.show()
 
 
 
-
-
-
-
-
-
-
-def find_version_table(file_name, tag):
-  f = open(file_name, 'r')
-  content = f.readlines();
- 
-  count = 0;
-  table = {}
-  regexp =  '.*' + tag + '.*Compute:(\w+)\s*id:\s*(\d+)\s*version_hash:\s*(\d+).*'
-  for line in content:
-    result = re.findall(regexp, line)
-    if (len(result) >= 1):
-      table[(result[0][0] + "-" + result[0][1])] = result[0][2]
-      count = count + 1
-    # else:
-    #   print "ERROR: corrupted line in " + file_name
-    #   print line
-  print "Read " + str(count) + " valid lines in " + file_name
-  return table
-
-
-def compare_version_tables(table_ref, table):
-  match = True
-  for key in table.keys():
-    if not table_ref.has_key(key):
-      print "ERROR: could not find key in refernce table: " + key
-      match = False
-    elif table_ref[key] != table[key]:
-      print "ERROR: the value does not match for the key: " + key
-      match = False
-  return match
 
 
 
