a4f5ed8b08290486d844bfed908f75114b29deac
Chinmayee Shah <chshah@stanford.edu>
2014-05-19 11:48:58 -0700

    added cache_var implementation. need to update all code that use cache_object now.

---
+++ data/cache/cache_var.cc
@@ -0,0 +1,219 @@
+/*
+ * Copyright 2013 Stanford University.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * - Neither the name of the copyright holders nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * A CacheVar is an application object corresponding to one nimbus
+ * variable, cached by the cache manager.
+ *
+ * Author: Chinmayee Shah <chshah@stanford.edu>
+ */
+
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "data/cache/cache_var.h"
+#include "data/cache/utils.h"
+#include "shared/dbg.h"
+#include "shared/geometric_region.h"
+#include "shared/nimbus_types.h"
+#include "worker/data.h"
+
+namespace nimbus {
+
+/**
+ * \details
+ */
+CacheVar::CacheVar() {}
+
+/**
+ * \details UpdateCache(...) finds out what data is in read_sets and not in
+ * CacheVar instance, and calls a ReadToCache(...) on the diff. Before
+ * reading the diff, UpdateCache(...) flushes out data that will be replaced
+ * (which is found based on geometric region of the data in cache and in
+ * read_set.
+ */
+void CacheVar::UpdateCache(const DataArray &read_set,
+                           const GeometricRegion &read_region) {
+    DataArray diff, flush;
+    for (size_t i = 0; i < read_set.size(); ++i) {
+        Data *d = read_set.at(i);
+        GeometricRegion dreg = d->region();
+        DMap::iterator it = data_map_.find(dreg);
+        if (it == data_map_.end()) {
+            // d->FlushToData(this);
+            diff.push_back(d);
+        } else {
+            Data *d_old = it->second;
+            if (d_old != d) {
+                // d->FlushToData(this);
+                diff.push_back(d);
+                // d_old->UnsetCacheObjectMapping(this);
+                if (write_back_.find(d) != write_back_.end()) {
+                    flush.push_back(d);
+                }
+            }
+        }
+    }
+    FlushCache(flush);
+    ReadToCache(diff, read_region);
+    for (size_t i = 0; i < diff.size(); ++i) {
+        Data *d = diff.at(i);
+        GeometricRegion dreg = d->region();
+        data_map_[dreg] = d;
+        // d->SetUpCacheObjectMapping(this);
+    }
+}
+
+/**
+ * \details SetUpWrite(...) sets up mapping for write data and the write
+ * region for a struct instance. It also sets up the write back data for the
+ * instance. Later, when the instance is released, these mappings and write
+ * region are used to pull data into nimbus data instances when needed.
+ */
+void CacheVar::SetUpWrite(const DataArray &write_set,
+                          const GeometricRegion &write_region) {
+    // TODO(Chinmayee): Twin data - imeplementation incomplete
+    DataArray diff, flush;
+    for (size_t i = 0; i < write_set.size(); ++i) {
+        Data *d = write_set.at(i);
+        GeometricRegion dreg = d->region();
+        DMap::iterator it = data_map_.find(dreg);
+        if (it == data_map_.end()) {
+            diff.push_back(d);
+        } else {
+            Data *d_old = it->second;
+            if (d_old != d) {
+                diff.push_back(d);
+                // d_old->UnsetCacheObjectMapping(this);
+                if (write_back_.find(d) != write_back_.end()) {
+                    flush.push_back(d);
+                }
+            }
+        }
+    }
+    FlushCache(flush);
+    for (size_t i = 0; i < diff.size(); ++i) {
+        Data *d = diff.at(i);
+        GeometricRegion dreg = d->region();
+        data_map_[dreg] = d;
+        write_back_.insert(d);
+        // d->SetUpCacheObjectMapping(this);
+        // d->SetUpDirtyCacheObjectMapping(this);
+    }
+}
+
+/**
+ * \details PullData(...) pulls data from cache, after locking the struct. This
+ * is like FlushToData(...), except that it also locks the struct. When data
+ * needs to be updated from outside CacheVar, use PullData.
+ */
+void CacheVar::PullData(Data *d) {
+    AcquireAccess(EXCLUSIVE);
+    if (write_back_.find(d) != write_back_.end()) {
+        DataArray write_set(1, d);
+        GeometricRegion dreg = d->region();
+        GeometricRegion wreg = GeometricRegion::
+            GetIntersection(write_region_, dreg);
+        WriteFromCache(write_set, wreg);
+        // d->clear_dirty_cache_object(this);
+        write_back_.erase(d);
+    }
+    ReleaseAccess();
+}
+
+/**
+ * \details GetDistance(...) gives the cost of using the CacheVar instance,
+ * given the read set. Current cost function is the sum of geometric sizes of
+ * data in the read set.
+ * The function does not have a separate argument for write set
+ * - if you want write set included in the cost function, either add  another
+ * argument or append it to read set that is passed to GetDistance.
+ */
+cache::distance_t CacheVar::GetDistance(const DataArray &read_set) const {
+    cache::distance_t cur_distance = 0;
+    for (size_t i = 0; i < read_set.size(); ++i) {
+        Data *d = read_set.at(i);
+        GeometricRegion dreg = d->region();
+        DMap::const_iterator it = data_map_.find(dreg);
+        if (it->second == d)
+            continue;
+        cur_distance += dreg.dx() * dreg.dy() * dreg.dz();
+    }
+    return cur_distance;
+}
+
+/**
+ * \details UnsetData(...) removes data d from data_map_.
+ */
+void CacheVar::UnsetData(Data *d) {
+    GeometricRegion dreg = d->region();
+    if (data_map_.find(dreg) != data_map_.end()) {
+        data_map_.erase(dreg);
+    }
+}
+
+/**
+ * \details FlushToData(...) flushes data from struct to given data. This
+ * function does not provide any locking.
+ * It should be used by methods of CacheVar only.
+ */
+void CacheVar::FlushToData(Data *d) {
+    if (write_back_.find(d) != write_back_.end()) {
+        DataArray write_set(1, d);
+        GeometricRegion dreg = d->region();
+        GeometricRegion wreg = GeometricRegion::
+            GetIntersection(write_region_, dreg);
+        WriteFromCache(write_set, wreg);
+        // d->clear_dirty_cache_object(this);
+        write_back_.erase(d);
+    }
+}
+
+/**
+ * \details FlushCache(...) flushes all data passed to it, The assumptions and
+ * semantics are similar to FlushToData(...). This function should be used by
+ * methods of CacheVar only.
+ */
+void CacheVar::FlushCache(const DataArray &flush_set) {
+    WriteFromCache(flush_set, write_region_);
+    for (size_t i = 0; i < flush_set.size(); ++i) {
+        Data *d = flush_set[i];
+        // d->clear_dirty_cache_object(this);
+        write_back_.erase(d);
+    }
+}
+
+}  // namespace nimbus
---
+++ data/cache/cache_var.h
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2013 Stanford University.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * - Neither the name of the copyright holders nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * A CacheVar is an application object corresponding to one nimbus
+ * variable, cached by the cache manager.
+ *
+ * Author: Chinmayee Shah <chshah@stanford.edu>
+ */
+
+#ifndef NIMBUS_DATA_CACHE_CACHE_VAR_H_
+#define NIMBUS_DATA_CACHE_CACHE_VAR_H_
+
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "data/cache/cache_object.h"
+#include "data/cache/utils.h"
+#include "shared/geometric_region.h"
+#include "shared/nimbus_types.h"
+#include "worker/data.h"
+
+namespace nimbus {
+
+typedef size_t type_id_t;
+typedef std::set<Data *> DataSet;
+
+/**
+ * \class CacheVar
+ * \details Application object corresponding to one numbus variable cached by
+ * the cache manager. A CacheVar can must be defined over a geometric region.
+ */
+class CacheVar : public CacheObject {
+    public:
+        /**
+         * \brief Creates a CacheVar
+         * \return Constructed CacheVar instance
+         */
+        explicit CacheVar();
+
+        /**
+         * \brief Creates a new CacheVar instance using current instance
+         * parameters
+         * \param struct_region specifies the spatial domain of the CacheVar
+         * instance
+         * \return Returns a pointer to the newly allocated CacheVar instance
+         * \details This is a virtual function that must be over-written by application
+         * writer. When CacheManager cannot satisfy an application object request,
+         * using the instances it has already cached, it calls CreateNew(..) on the
+         * prototype passed in the request.
+         */
+        virtual CacheVar *CreateNew(const GeometricRegion &struct_region) const = 0;
+
+        /**
+         * \brief Updates CacheVar with data from read_set - performs update
+         * only for those physical data that have changed since the CacheVar
+         * instance was last updated
+         * \param read_set is a data array corresponding to nimbus variables
+         * \param read_region is the geometric region to read
+         */
+        void UpdateCache(const DataArray &read_set,
+                         const GeometricRegion &read_region);
+
+        /**
+         * \brief Sets up mappings for data in write_sets, and also sets up the
+         * write region
+         * \param write_set is a data array corresponding to nimbus variables
+         * \param write_region is the geometric region to write
+         */
+        void SetUpWrite(const DataArray &write_sets,
+                        const GeometricRegion &write_region);
+
+        /**
+         * \brief Pulls data from cache, removes corresponding dirty data
+         * mapping. Locks the cache object when pulling the data.
+         * \param d is data to flush to
+         */
+        virtual void PullData(Data *d);
+
+        /**
+         * \brief Calculates distance of a CacheVar, from the given read_set.
+         * This distance indicates the cost of reconstruction if
+         * this CacheVar instance is used.
+         * \param read_set is a data array corresponding to nimbus variables
+         * \return Returns distance (cost)
+         */
+        cache::distance_t GetDistance(const DataArray &read_set) const;
+
+        /**
+         * \brief Unsets mapping between data and CacheVar instance
+         * \param d denotes the data to unmap
+         */
+        virtual void UnsetData(Data *d);
+
+    private:
+        /**
+         * \brief Flushes data from cache, removes corresponding dirty data
+         * mapping
+         * \param d is data to flush to
+         */
+        void FlushToData(Data *d);
+
+        /**
+         * \brief Flushes data from cache to data in flush_set (immediately)
+         * \param flush_set is a data array corresponding to nimbus variables
+         */
+        void FlushCache(const DataArray &flush_set);
+
+        // cache-data mappings
+        typedef std::map<GeometricRegion,
+                         Data *> DMap;
+        DMap data_map_;
+        DataSet write_back_;
+
+    protected:
+        /**
+         * \brief Reads data from read_sets into CacheVar instance
+         * \param read_set is a data array corresponding to nimbus variables
+         * \param read_region is the geometric region to read
+         * \details This function must be overwritten by the application
+         * writer. It provides the transformation from a set of nimbus data to
+         * (application) cached instance.
+         */
+        virtual void ReadToCache(const DataArray &read_set,
+                                 const GeometricRegion &read_region) = 0;
+
+        /**
+         * \brief Writes data from CacheVar instance to write_sets
+         * \param write_set is a data array corresponding to nimbus variables
+         * \param write_region is the geometric region to write
+         * \details This function must be overwritten by the application
+         * writer. It provides the transformation from (application) cached
+         * instance to nimbus data.
+         */
+        virtual void WriteFromCache(const DataArray &write_set,
+                                    const GeometricRegion &write_region) = 0;
+};  // class CacheVar
+
+typedef std::vector<CacheVar *> CacheVars;
+
+}  // namespace nimbus
+
+#endif  // NIMBUS_DATA_CACHE_CACHE_VAR_H_
