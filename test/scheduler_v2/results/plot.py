32cd23370cd0ca306089afe362d375ae25a6381c
Omid Mashayekhi <omidm@stanford.edu>
2014-06-25 15:58:53 -0700

    added results of experiment after activating the scheduler new reference counter through version manager, it works great :)

--- test/scheduler_v2/results/plot.py
+++ test/scheduler_v2/results/plot.py
@@ -11,9 +11,11 @@ from matplotlib.font_manager import FontProperties
 from decimal import *
 getcontext().prec = 2
 
-P = 4
+N = 4
 
-X = (128, 256, 512, 1024)
+
+# collected data
+I = (88 - 142, 150 - 203, 211 - 262, 271 - 321, 330 - 382, 393 - 449)
 
 VV = []
 VV.append((0.68, 0.61, 0.66, 0.67, 0.57))
@@ -26,27 +28,65 @@ for i in range(0, len(VV)):
   V.append(np.mean(VV[i]))
   E.append(np.std(VV[i]))
 
-I = (88 - 142, 150 - 203, 211 - 262, 271 - 321, 330 - 382, 393 - 449)
+AO = (1 * 128 / 10, 2.4 * 256 / 10, 5.1 * 512 / 10, 15.68 * 1024 / 10)
 
-A = (1 * 128 / 10, 2.4 * 256 / 10, 5.1 * 512 / 10, 15.68 * 1024 / 10)
+AN = (0.05 * 128 / 10, 0.043 * 256 / 10, 0.041 * 512 / 10, 0.044 * 1024 / 10)
 
 J = (7.57, 22.73, 79.80, 287.78)
 
-a = plt.errorbar(X, A, fmt='s-', color='r', ms=8)
-j = plt.errorbar(X, J, fmt='^-', color='b', ms=8)
-v = plt.errorbar(X, V, fmt='o-', color='g', ms=8)
-hline = plt.axhline(-np.mean(I), color='#fb8072', ls='--', linewidth=4)
 
-plt.legend((a, j, v, hline),
-    ('Assigning First Stage', 'Spawning Jobs', 'Versioning Job Graph', 'Iteration Duration (128 cube)'),
-    loc='upper left')
+# prepare data for plotting
+X = (128, 256, 512, 1024)
+
+Y = []
+Y.append(AO)
+Y.append(AN)
+Y.append(J)
+Y.append(V)
+
+Colors = []
+Colors.append('r')
+Colors.append('r')
+Colors.append('b')
+Colors.append('g')
+
+Formats = []
+Formats.append('s-')
+Formats.append('s--')
+Formats.append('^-')
+Formats.append('o-')
+
+Legends = []
+Legends.append('Assigning First Stage (Old)')
+Legends.append('Assigning First Stage (New)')
+Legends.append('Spawning Jobs')
+Legends.append('Versioning Job Graph')
+
+P = []
+
+for i in range(0, len(Y)):
+  P.append(plt.errorbar(X, Y[i], fmt=Formats[i], color=Colors[i], ms=8))
+  for j in range(0, N):
+    tag = '%2.2f' % Y[i][j]
+    plt.annotate(tag, xy=(X[j], Y[i][j]))
+
+
+Legends.append('Iteration Duration (128 cube)')
+P.append(plt.axhline(-np.mean(I), color='#fb8072', ls='--', linewidth=4))
+tag = '%2.2f' % (-np.mean(I))
+plt.annotate(tag, xy=(X[0], (-np.mean(I))))
+
+plt.legend(P, Legends, loc='upper left')
+
 plt.yscale('log')
 plt.xscale('log')
-plt.xlim(100, 1025)
+plt.xlim(100, 1024 * 1.5)
 
 plt.ylabel('Time (seconds)')
 plt.xlabel('Number of Application Partitions')
 plt.title('Analyzing Scalability of Nimbus in Water Simulation')
+
+
 
 # plt.grid()
 plt.savefig("test.png")
