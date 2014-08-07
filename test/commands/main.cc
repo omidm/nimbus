0740fe7fc23e25e0290e9e5b0af3e72db065ae6d
Philip Levis <pal@sing.stanford.edu>
2014-08-07 02:40:10 -0700

    Better command testing.

--- test/commands/main.cc
+++ test/commands/main.cc
@@ -43,40 +43,61 @@
 #include "shared/id.h"
 #include "shared/idset.h"
 #include "shared/spawn_compute_job_command.h"
+#include "shared/spawn_copy_job_command.h"
 
 using namespace nimbus; // NOLINT
 
+IDSet<logical_data_id_t> ldo_set_a;
+IDSet<logical_data_id_t> ldo_set_b;
+IDSet<job_id_t> job_set_a;
+IDSet<job_id_t> job_set_b;
+ID<logical_data_id_t> ldo_a;
+ID<logical_data_id_t> ldo_b;
+ID<job_id_t> job_id;
+ID<job_id_t> parent;
+ID<job_id_t> future;
+bool sterile;
+Parameter params;
+std::string name = "test-string";
+
+void initalize_sets() {
+  for (int i = 0; i < 125; i++) {
+    ldo_set_a.insert(10000 + i);
+  }
+  for (int i = 0; i < 119; i++) {
+    ldo_set_b.insert(11000 + i);
+  }
+  for (int i = 0; i < 21; i++) {
+    job_set_a.insert(20000 + i);
+  }
+  for (int i = 0; i < 34; i++) {
+    job_set_b.insert(21000 + i);
+  }
+
+  ldo_a.set_elem(10125);
+  ldo_b.set_elem(11119);
+  job_id.set_elem(2014);
+  parent.set_elem(1940);
+  future.set_elem(1977);
+  sterile = false;
+
+  SerializedData d("serialized data");
+  params.set_ser_data(d);
+}
+
+
 int test_compute_job() {
-  std::string name = "compute-test";
-  ID<job_id_t> id(53);
-  IDSet<logical_data_id_t> read;
-  for (int i = 0; i < 125; i++) {
-    read.insert(2000 + i);
-  }
-  IDSet<logical_data_id_t> write;
-  for (int i = 0; i < 125; i++) {
-    write.insert(1000 + i);
-  }
-  IDSet<job_id_t> before;
-  before.insert(43);
-  IDSet<job_id_t> after;
-
-  ID<job_id_t> parent(23);
-  ID<job_id_t> future(0);
-
-  bool sterile = false;
-  const Parameter params;
-
+  std::cout << "Testing SpawnComputeJob." << std::endl;
   SpawnComputeJobCommand cmd((const std::string)name,
-                             (const ID<job_id_t>)id,
-                             (const IDSet<logical_data_id_t>)read,
-                             (const IDSet<logical_data_id_t>)write,
-                             (const IDSet<job_id_t>)before,
-                             (const IDSet<job_id_t>)after,
+                             (const ID<job_id_t>)job_id,
+                             (const IDSet<logical_data_id_t>)ldo_set_a,
+                             (const IDSet<logical_data_id_t>)ldo_set_b,
+                             (const IDSet<job_id_t>)job_set_a,
+                             (const IDSet<job_id_t>)job_set_b,
                              (const ID<job_id_t>)parent,
                              (const ID<job_id_t>)future,
                              (const bool)sterile,
-                             params);
+                             (const Parameter)params);
 
   std::string toStringResult = cmd.toString();
   SchedulerPBuf buf;
@@ -101,8 +122,72 @@ int test_compute_job() {
   }
 }
 
-int main(int argc, char** argv) {
-  test_compute_job();
+int test_copy_job() {
+  std::cout << "Testing SpawnCopyJob." << std::endl;
   
+  SpawnCopyJobCommand cmd((const ID<job_id_t>)job_id,
+                          (const ID<logical_data_id_t>)ldo_a,
+                          (const ID<logical_data_id_t>)ldo_b,
+                          (const IDSet<job_id_t>)job_set_a,
+                          (const IDSet<job_id_t>)job_set_b,
+                          (const ID<job_id_t>)parent);
+
+  std::string toStringResult = cmd.toString();
+  SchedulerPBuf buf;
+  buf.ParseFromString(toStringResult);
+  SpawnCopyJobCommand cmd2;
+  bool result = cmd2.Parse(buf);
+  
+  if (result) {
+    std::string tags = cmd2.toStringWTags();
+    std::cout << "Successful parse of " << result << std::endl;
+    std::cout << "With tags: " << tags.size() << ", protobuf: " << toStringResult.size() << std::endl;
+    std::cout << "Tags: " << tags << std::endl;
+    return 1;
+  } else {
+    std::cout << "Failed parse of buffer" << std::endl;
+    std::cout << cmd2.toStringWTags() << std::endl;
+    const unsigned char* buf = (const unsigned char*)toStringResult.c_str();
+    for (unsigned int i = 0; i < toStringResult.size(); i++) {
+      printf("%02x ", buf[i]);
+    }
+    return 0;
+  }
+  return 1;
+}
+int test_define_data() {
+  return 1;
+}
+int test_handshake() {
+  return 1;
+}
+int test_jobdone() {
+  return 1;
+}
+int test_execute_job() {
+  return 1;
+}
+int test_define_partition() {
+  return 1;
+}
+int test_terminate() {
+  return 1;
+}
+int test_profile() {
+  return 1;
+}
+
+
+int main(int argc, char** argv) {
+  initalize_sets();
+  test_compute_job();
+  test_copy_job();
+  test_define_data();
+  test_handshake();
+  test_jobdone();
+  test_execute_job();
+  test_define_partition();
+  test_terminate();
+  test_profile();
   return 0;
 }
