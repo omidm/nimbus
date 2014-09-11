65f38f4cb26a325108e1768c0a33dcd2b0fab6c1
Hang Qu(sing-pc) <quhang@stanford.edu>
2014-09-11 10:44:17 -0700

    Commit the script to generate plots. Readme will be added later.

--- test/ec2/bottleneck_test/trim.py
+++ test/ec2/bottleneck_test/trim.py
@@ -4,7 +4,7 @@ import sys
 import re
 
 f = open(sys.argv[1], 'r')
-g = open("../result/" + sys.argv[1], 'w')
+g = open(sys.argv[2], 'w')
 contents = f.readlines()
 for line in contents[:-1]:
 	g.write(line)
