fb65a6327369897932251fdcd0eed2361b770761
Omid Mashayekhi <omidm@stanford.edu>
2014-09-16 14:44:19 -0700

    updated the tcp conf script no to change any system file.

--- scripts/configure_tcp.sh
+++ scripts/configure_tcp.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-sudo echo '# These changes are added to improme the the tcp performance. - omidm' >> /etc/sysctl.conf
-sudo echo 'net.core.wmem_max = 536870912' >> /etc/sysctl.conf
-sudo echo 'net.core.rmem_max = 536870912' >> /etc/sysctl.conf
-sudo echo 'net.ipv4.tcp_rmem = 10240 102400 536870912' >> /etc/sysctl.conf
-sudo echo 'net.ipv4.tcp_wmem = 10240 102400 536870912' >> /etc/sysctl.conf
-sudo echo 'net.ipv4.tcp_slow_start_after_idle = 0' >> /etc/sysctl.conf
-sudo sysctl -p
+sudo echo '# These changes are added to improme the the tcp performance. - omidm'
+sudo sysctl -w net.core.wmem_max=536870912
+sudo sysctl -w net.core.rmem_max=536870912
+sudo sysctl -w net.ipv4.tcp_rmem='10240 102400 536870912'
+sudo sysctl -w net.ipv4.tcp_wmem='10240 102400 536870912'
+sudo sysctl -w net.ipv4.tcp_slow_start_after_idle=0
 
