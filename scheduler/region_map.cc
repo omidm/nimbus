7383ba318d02867b55ea302c472d706c609521c0
Omid Mashayekhi <omidm@stanford.edu>
2014-09-30 16:31:09 -0700

    added partitioning strategy for 100 worker to region map.

--- scheduler/region_map.cc
+++ scheduler/region_map.cc
@@ -457,6 +457,11 @@ void RegionMap::SplitDimensions(size_t worker_num, size_t *num_x, size_t *num_y,
       *num_y = 1;
       *num_z = 1;
       break;
+    case 100 :
+      *num_x = 5;
+      *num_y = 5;
+      *num_z = 4;
+      break;
     default:
       dbg(DBG_ERROR, "ERROR: Do not know how to split!");
       exit(-1);
