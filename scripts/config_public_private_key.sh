e90f4085c3a9616b91ddd06637a9e3e33fc36fd1
Omid Mashayekhi <omidm@stanford.edu>
2014-11-27 05:36:36 -0800

    updated the script to generate the rsa key-pair for db communications.

--- scripts/config_public_private_key.sh
+++ scripts/config_public_private_key.sh
@@ -2,5 +2,9 @@
 
 mkdir -p ~/.ssh 
 touch ~/.ssh/authorized_keys
+
+# create the key value pair
+ssh-keygen -t rsa -N "" -f nimbus-rsa-key-pair
+
 cat ~/cloud/src/nimbus/scripts/nimbus-rsa-key-pair.pub >> ~/.ssh/authorized_keys
 
