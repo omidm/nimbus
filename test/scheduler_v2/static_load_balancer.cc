0269ba0e2d0baf536e2b2e88a0e1b291e65932fb
Omid Mashayekhi <omidm@stanford.edu>
2015-01-27 22:53:15 -0800

    8 worker case for static load balancer, uniform now

--- test/scheduler_v2/static_load_balancer.cc
+++ test/scheduler_v2/static_load_balancer.cc
@@ -305,9 +305,9 @@ void StaticLoadBalancer::SplitDimensions(size_t worker_num,
       *num_z = 1;
       break;
     case 8 :
-      *num_x = 8;
-      *num_y = 1;
-      *num_z = 1;
+      *num_x = 2;
+      *num_y = 2;
+      *num_z = 2;
       break;
     case 100 :
       *num_x = 5;
