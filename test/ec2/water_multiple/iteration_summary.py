43f8a023c40a4cef45dc9deb566e1b169a166e24
Omid Mashayekhi <omidm@stanford.edu>
2015-05-19 19:28:59 -0700

    new scripts that I used for new measurements.

--- test/ec2/water_multiple/iteration_summary.py
+++ test/ec2/water_multiple/iteration_summary.py
@@ -3,10 +3,51 @@
 # ./get_summary.py [directory_name] [number of workers]
 
 import sys
+import argparse
 
 def parse_line(line):
     items = line.split()
     return float(items[1]),  float(items[3]), float(items[5])
+
+
+## Parse the command line arguments ##
+parser = argparse.ArgumentParser(description='Process log files.')
+parser.add_argument(
+    "-d", "--dir_path",
+    dest="dirpath",
+    help="input dorectory path")
+parser.add_argument(
+    "-wn", "--worker_num",
+    dest="workernum",
+    default=8,
+    help="worker num")
+parser.add_argument(
+    "-cn", "--core_num",
+    dest="corenum",
+    default=8,
+    help="core num per worker")
+parser.add_argument(
+    "-ti", "--truncate_index",
+    dest="truncateindex",
+    default=0,
+    help="truncate index to ignore the initial iterations")
+parser.add_argument(
+    "-v", "--verbose",
+    dest="collapse",
+    action="store_true",
+    help="print per iteration stats as well")
+
+
+args = parser.parse_args()
+
+d  = args.dirpath 
+N  = int(args.workernum) 
+CN = int(args.corenum)
+TI = int(args.truncateindex)
+
+
+
+
 
 total_run_sum   = 0;
 total_block_sum = 0;
@@ -14,11 +55,12 @@ total_idle_sum  = 0;
 total_total_sum = 0;
 iter_nums = []
     
-print '   worker    steps      run    block     idle    total'
+print '--------------------------------------------------------------------------------------'
+print '   worker    steps                        run      block     idle    synch     total'
+print '--------------------------------------------------------------------------------------'
 
-N = int(sys.argv[2])
 for rank in range(1, N+1):
-  f = open('{}/{}_main_timers.txt'.format(sys.argv[1], rank), 'r')
+  f = open('{}/{}_main_timers.txt'.format(d, rank), 'r')
 
   iter_num  = 0
   run_sum   = 0;
@@ -29,9 +71,11 @@ for rank in range(1, N+1):
   idx = 0
   for line in f:
       idx += 1
-      if idx == 1:
-        continue
       run, block, idle = parse_line(line)
+      if (args.collapse):
+        print '          {:8.0f}                   {:8.2f}   {:8.2f} {:8.2f} {:8.2f}  {:8.2f}'.format(idx, run/CN, block/CN, idle/CN, (block+idle)/CN, (idle+block+run)/CN)
+      if idx <= TI:
+        continue
       iter_num  += 1
       run_sum   += run
       block_sum += block
@@ -39,12 +83,14 @@ for rank in range(1, N+1):
       total_sum  += run + block + idle
 
   iter_nums.append(iter_num)
-  total_run_sum   += run_sum
-  total_block_sum += block_sum
-  total_idle_sum  += idle_sum
-  total_total_sum  += total_sum
+  total_run_sum   += run_sum/iter_num
+  total_block_sum += block_sum/iter_num
+  total_idle_sum  += idle_sum/iter_num
+  total_total_sum  += total_sum/iter_num
 
-  print '{:8.0f}: {:8.0f} {:8.2f} {:8.2f} {:8.2f} {:8.2f}'.format(
-            rank, iter_num, run_sum/8, block_sum/8, idle_sum/8, total_sum/8)
-print '---------------------------------------------------------'
-print ' Average: {:8.0f} {:8.2f} {:8.2f} {:8.2f} {:8.2f}'.format(iter_nums[0], total_run_sum/N/8, total_block_sum/N/8, total_idle_sum/N/8, total_total_sum/N/8)
+  print '{:8.0f}: {:8.0f}                   {:8.2f}   {:8.2f} {:8.2f} {:8.2f}  {:8.2f}'.format(
+            rank, iter_num, run_sum/CN/iter_num, block_sum/CN/iter_num, idle_sum/CN/iter_num, (block_sum+idle_sum)/CN/iter_num, total_sum/CN/iter_num)
+
+print '--------------------------------------------------------------------------------------'
+print ' Average: {:8.0f}                   {:8.2f}   {:8.2f} {:8.2f} {:8.2f}  {:8.2f}'.format(iter_nums[0], total_run_sum/N/CN, total_block_sum/N/CN, total_idle_sum/N/CN, (total_block_sum+total_idle_sum)/N/CN, total_total_sum/N/CN)
+print '--------------------------------------------------------------------------------------'
