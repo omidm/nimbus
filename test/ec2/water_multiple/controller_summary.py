446d087b647be3083fd4652f43792fbafeac3c17
Omid Mashayekhi <omidm@stanford.edu>
2015-05-21 18:34:00 -0700

    script to process the controller per iteration stats.

---
+++ test/ec2/water_multiple/controller_summary.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+# ./get_summary.py [directory_name] [number of workers]
+
+import sys
+import argparse
+import numpy as np
+
+def parse_line(line):
+    items = line.split()
+    return float(items[0]), float(items[3]), float(items[5]), float(items[7])
+
+def sum(list):
+  sum = 0
+  for element in list:
+    sum += element
+  return sum
+
+def print_bar(list, max_len):
+  m = max(list)
+  for element in list:
+    rod = "*"
+    for i in range(0, int(max_len * element / m)):
+      rod += "*"
+    print rod
+
+## Parse the command line arguments ##
+parser = argparse.ArgumentParser(description='Process log files.')
+parser.add_argument(
+    "-d", "--dir_path",
+    dest="dirpath",
+    help="input dorectory path")
+parser.add_argument(
+    "-ti", "--truncate_index",
+    dest="truncateindex",
+    default=0,
+    help="truncate index to ignore the initial iterations")
+parser.add_argument(
+    "-bl", "--bar_length",
+    dest="barlength",
+    default=0,
+    help="the maximum length of the the printed bar per iteration that shows iteration length")
+
+args = parser.parse_args()
+d  = args.dirpath 
+TI = int(args.truncateindex)
+BL = int(args.barlength)
+
+
+
+
+
+f = open('{}/controller_stats.txt'.format(d), 'r')
+
+bytes_sent       = [];
+bytes_received   = [];
+overhead_time    = [];
+iteration_length = []
+last_time_stamp  = 0;
+iter_num         = 0
+
+
+for line in f:
+    iter_num  += 1
+    stamp, sent, received, overhead = parse_line(line)
+
+    if (iter_num > 1):
+      iteration_length.append(stamp - last_time_stamp)
+
+    last_time_stamp = stamp
+    overhead_time.append(overhead)
+    bytes_sent.append(sent)
+    bytes_received.append(received)
+
+iteration_length = [np.mean(iteration_length)] + iteration_length
+
+if (BL > 0):
+  print_bar(iteration_length, BL)
+
+
+print "------------------------------------------------------------------------------"
+print "Truncated the first {:d} iteartions for the following stats:".format(TI)
+print "------------------------------------------------------------------------------"
+
+print "Average iteration duration | {:8.2f} (s)".format(np.mean(iteration_length[TI: iter_num]))
+print "Average overhead           | {:8.2f} (s)".format(np.mean(overhead_time[TI: iter_num]))
+print "Average bytes sent         | {:8.2f} (MB)".format(np.mean(bytes_sent[TI: iter_num]))
+print "Average bytes received     | {:8.2f} (MB)".format(np.mean(bytes_received[TI: iter_num]))
+print "------------------------------------------------------------------------------"
+
+print "\n------------------------------------------------------------------------------"
+print "Total bytes sent           | {:8.2f} (MB)".format(sum(bytes_sent))
+print "Total bytes received       | {:8.2f} (MB)".format(sum(bytes_received))
+print "------------------------------------------------------------------------------"
+
