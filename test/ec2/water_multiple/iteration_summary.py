eb99a9a427fcfbbd3d5bec6582539b8c9b845e98
Omid Mashayekhi <omidm@stanford.edu>
2015-06-02 21:45:41 -0700

    updatde the script to process the new log, backward compatible.

--- test/ec2/water_multiple/iteration_summary.py
+++ test/ec2/water_multiple/iteration_summary.py
@@ -7,7 +7,12 @@ import argparse
 
 def parse_line(line):
     items = line.split()
-    return float(items[1]),  float(items[3]), float(items[5])
+    pexec = 0
+    if len(items) == 8:
+      pexec = float(items[7])
+    else:
+      pexec = float('nan')
+    return float(items[1]),  float(items[3]), float(items[5]), pexec
 
 
 ## Parse the command line arguments ##
@@ -49,48 +54,54 @@ TI = int(args.truncateindex)
 
 
 
-total_run_sum   = 0;
-total_block_sum = 0;
-total_idle_sum  = 0;
-total_total_sum = 0;
+total_parent_sum = 0;
+total_run_sum    = 0;
+total_block_sum  = 0;
+total_idle_sum   = 0;
+total_total_sum  = 0;
 iter_nums = []
     
 print '--------------------------------------------------------------------------------------'
-print '   worker    steps                        run      block     idle    synch     total'
+print '   worker    steps        parent          run      block     idle    synch     total'
 print '--------------------------------------------------------------------------------------'
 
 for rank in range(1, N+1):
   f = open('{}/{}_main_timers.txt'.format(d, rank), 'r')
 
-  iter_num  = 0
-  run_sum   = 0;
-  block_sum = 0;
-  idle_sum  = 0;
-  total_sum = 0;
+  iter_num   = 0
+  parent_sum = 0;
+  run_sum    = 0;
+  block_sum  = 0;
+  idle_sum   = 0;
+  total_sum  = 0;
 
   idx = 0
   for line in f:
       idx += 1
-      run, block, idle = parse_line(line)
+      run, block, idle, parent = parse_line(line)
       if (args.collapse):
-        print '          {:8.0f}                   {:8.2f}   {:8.2f} {:8.2f} {:8.2f}  {:8.2f}'.format(idx, run/CN, block/CN, idle/CN, (block+idle)/CN, (idle+block+run)/CN)
+        print '          {:8.0f}      {:8.2f}     {:8.2f}   {:8.2f} {:8.2f} {:8.2f}  {:8.2f}'.format(
+            idx, parent, run/CN, block/CN, idle/CN, (block+idle)/CN, (idle+block+run)/CN)
       if idx <= TI:
         continue
       iter_num  += 1
-      run_sum   += run
-      block_sum += block
-      idle_sum  += idle
+      parent_sum += parent
+      run_sum    += run
+      block_sum  += block
+      idle_sum   += idle
       total_sum  += run + block + idle
 
   iter_nums.append(iter_num)
-  total_run_sum   += run_sum/iter_num
-  total_block_sum += block_sum/iter_num
-  total_idle_sum  += idle_sum/iter_num
+  total_parent_sum += parent_sum/iter_num
+  total_run_sum    += run_sum/iter_num
+  total_block_sum  += block_sum/iter_num
+  total_idle_sum   += idle_sum/iter_num
   total_total_sum  += total_sum/iter_num
 
-  print '{:8.0f}: {:8.0f}                   {:8.2f}   {:8.2f} {:8.2f} {:8.2f}  {:8.2f}'.format(
-            rank, iter_num, run_sum/CN/iter_num, block_sum/CN/iter_num, idle_sum/CN/iter_num, (block_sum+idle_sum)/CN/iter_num, total_sum/CN/iter_num)
+  print '{:8.0f}: {:8.0f}      {:8.2f}     {:8.2f}   {:8.2f} {:8.2f} {:8.2f}  {:8.2f}'.format(
+            rank, iter_num, parent_sum/iter_num, run_sum/CN/iter_num, block_sum/CN/iter_num, idle_sum/CN/iter_num, (block_sum+idle_sum)/CN/iter_num, total_sum/CN/iter_num)
 
 print '--------------------------------------------------------------------------------------'
-print ' Average: {:8.0f}                   {:8.2f}   {:8.2f} {:8.2f} {:8.2f}  {:8.2f}'.format(iter_nums[0], total_run_sum/N/CN, total_block_sum/N/CN, total_idle_sum/N/CN, (total_block_sum+total_idle_sum)/N/CN, total_total_sum/N/CN)
+print ' Average: {:8.0f}      {:8.2f}     {:8.2f}   {:8.2f} {:8.2f} {:8.2f}  {:8.2f}'.format(
+    iter_nums[0], total_parent_sum, total_run_sum/N/CN, total_block_sum/N/CN, total_idle_sum/N/CN, (total_block_sum+total_idle_sum)/N/CN, total_total_sum/N/CN)
 print '--------------------------------------------------------------------------------------'
