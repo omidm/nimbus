b9842ece3ec1e17eb54cd37c3813fb3215793dbd
Omid Mashayekhi <omidm@stanford.edu>
2016-02-11 16:32:16 -0800

    added script to stop nimbus workers.

---
+++ scripts/stop-workers.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+#
+#  Copyright 2013 Stanford University.
+#  All rights reserved.
+# 
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+# 
+#  - Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 
+#  - Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the
+#    distribution.
+# 
+#  - Neither the name of the copyright holders nor the names of
+#    its contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+# 
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#  FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+#  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+#  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+#  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+#  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+#  OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# Stops Nimbus workers on the machine this script is executed on.
+
+# Author: Omid Mashayekhi <omidm@stanford.edu>
+
+# **************************
+# Text Reset
+RCol='\x1B[0m'    
+# Regular           
+Bla='\x1B[0;30m';
+Red='\x1B[0;31m';
+Gre='\x1B[0;32m';
+Yel='\x1B[0;33m';
+Blu='\x1B[0;34m';
+Pur='\x1B[0;35m';
+Cya='\x1B[0;36m';
+Whi='\x1B[0;37m';
+# **************************
+
+CONTROLLER_BIN="nimbus_worker"
+LOG_DIR="${NIMBUS_HOME}/logs"
+ARGS="$@"
+
+
+
+if [[ ${ARGS} = *--help* ]] || [[ ${ARGS} = *-h* ]]; then
+  echo -e "${Blu}Stops the nimbus workers on the machine this script is executed on."
+  echo -e "Usage: ./scripts/stop-workers.sh"
+  echo -e "${RCol}"
+  exit 1
+fi
+
+killall -v ${CONTROLLER_BIN} &> /dev/null
+
+SUCCESS="$?"
+
+if [ ${SUCCESS} != "0" ]; then
+  echo -e "${Red}No worker found to stop!${RCol}"
+else
+  echo -e "${Gre}Stoped the workers.${RCol}"
+fi
+
